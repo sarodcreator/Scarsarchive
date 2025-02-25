d53968e9a69ee70f9c864a0e7161ebdd49b5a044
Saro <sarobaridoo@gmail.com>
2025-02-25 21:32:54 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './home/index';
+import Gallery from './gallery/index';
+import Contact from './contact/index';
+
+const appRoutes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/gallery',
+    element: <Gallery />
+  },
+  {
+    path: '/contact-us',
+    element: <Contact />
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={appRoutes} />
     </div>
-  );
+  )
 }
 
 export default App;
