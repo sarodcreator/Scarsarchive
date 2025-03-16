0ccec25cf219c3e2939e199073e26792345ab33f
Saro <sarobaridoo@gmail.com>
2025-03-16 08:34:58 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import {  RouterProvider } from 'react-router-dom';
-import Routes from './Routes/index';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../home/index';
+import Gallery from '../gallery/index';
 
-function App() {
-  return (
-    <div>
-      <RouterProvider router={Routes} />
-    </div>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/gallery',
+    element: <Gallery />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
