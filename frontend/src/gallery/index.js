cfa17122073cb1b8dca1fb1ce9bade04c8384bea
Saro <sarobaridoo@gmail.com>
2025-03-16 08:33:14 +0100

    Update index.js

--- frontend/src/gallery/index.js
+++ frontend/src/gallery/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import './index.css';
 import Navbar from '../components/navbar';
 import GallerySlide from './galleryslide';
@@ -27,3 +27,43 @@ const index = () => {
 }
 
 export default index;
+*/
+import React, { useState } from 'react';
+import './index.css';
+import Navbar from '../components/navbar';
+import GallerySlide from './galleryslide';
+import Menu from './menu';
+import Testimonial from './review';
+import More from './moreWorks';
+import Footer from '../components/footer';
+
+const Index = () => {
+  const [selectedCategory, setSelectedCategory] = useState('wedding'); // Default category
+
+  // Handle category change
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+
+    // Smooth scroll to gallery section
+    document.querySelector('.gallery-slide').scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="gallery-slide">
+        <div>
+          <Menu onCategoryChange={handleCategoryChange} />
+        </div>
+        <div>
+          <GallerySlide selectedCategory={selectedCategory} />
+        </div>
+      </div>
+      <Testimonial />
+      <More />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
